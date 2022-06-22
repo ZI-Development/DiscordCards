9d3dfd9f5e523070dd50ec08c47ba9831384f44f
Glitch (getting-started-discord) <none>
2022-06-22 19:46:30 +0000

    🏣⛑ Checkpoint ./app.js:46126351/1365 ./.glitch-assets:46126351/1181

--- app.js
+++ app.js
@@ -60,24 +60,56 @@ app.post('/interactions', async function (req, res) {
         },
       });
     }
-    if(name === 'penis') {
+    if (name === 'challenge' && id) {
+      const userId = req.body.member.user.id;
+      // User's object choice
+      const objectName = req.body.data.options[0].value;
+
+      // Create active game using message ID as the game ID
+      activeGames[id] = {
+        id: userId,
+        objectName,
+      };
+
       return res.send({
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
         data: {
-          content: '8=====>'  
+          // Fetches a random emoji to send from a helper function
+          content: `Rock papers scissors challenge from <@${userId}>`,
+          components: [
+            {
+              type: MessageComponentTypes.ACTION_ROW,
+              components: [
+                {
+                  type: MessageComponentTypes.BUTTON,
+                  // Append the game ID to use later on
+                  custom_id: `accept_button_${req.body.id}`,
+                  label: 'Accept',
+                  style: ButtonStyleTypes.PRIMARY,
+                },
+              ],
+            },
+          ],
         },
       });
     }
-    if(name === 'glad_to_see_u') {
-        
+    if(name === 'penis') {      
+      const exampleEmbed = new MessageEmbed()
+        .setImage('https://cdn.glitch.global/90bcdd4c-d30a-4fb8-89d2-11bb34f0fbde/penis.png?v=1655920368058');
+      
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          embeds: [exampleEmbed]
+        },
+      });
+    }
+    if(name === 'glad_to_see_u') {        
       return res.send({
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
         data: {
           content: 'Me too',     
-          ephemeral: true,
-          embeds: [exampleEmbed]
-        }
-        
+        }        
       });
     }
   }
