b07ff0a81f10f8fac818d205e0db146e9938589a
Glitch (getting-started-discord) <none>
2022-06-22 20:21:20 +0000

    🐃⛰ Checkpoint ./star-realms/cards.js:46126351/985 ./.glitch-assets:46128679/19133

---
+++ star-realms/cards.js
@@ -0,0 +1,5 @@
+let cards = [
+  {name: 'Mothership',  url:'https://cdn.glitch.global/90bcdd4c-d30a-4fb8-89d2-11bb34f0fbde/Mothership.png?v=1655926882042'},
+  {name: 'Battle blob', url:'https://cdn.glitch.global/90bcdd4c-d30a-4fb8-89d2-11bb34f0fbde/Battle%20Blob.png?v=1655926885644'},
+  {name: 'Battle blob', url:'https://cdn.glitch.global/90bcdd4c-d30a-4fb8-89d2-11bb34f0fbde/Blob%20Fighter.png?v=1655926889816'},
+]
